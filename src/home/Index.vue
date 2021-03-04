7a6928f1871e218fcc707717eed3e7937230e2fd
zhangran <zranspring@163.com>
2021-03-04 22:38:33 +0800

    用户管理界面优化

--- src/home/Index.vue
+++ src/home/Index.vue
@@ -1,75 +1,79 @@
 <template>
   <div class="home-wrapper">
-    <div class="design">
-      <div class="text">0041号阀门</div>
-      <div class="valve-wrapper">
-        <img class="valve" src="@/assets/images/valve-open.png" alt="" />
-        <div class="time-wrapper">
-          <p class="start-time">14:25</p>
-          <p class="end-time"></p>
-        </div>
-      </div>
-      <van-divider />
-      <div class="option-wrapper">
-        <div class="water">
-          <span>浇水</span>
-          <van-switch :value="waterOpen" @input="waterHandler" />
+    <div v-if="!finished">
+      <div class="design">
+        <div class="text">0041号阀门</div>
+        <div class="valve-wrapper">
+          <img class="valve" src="@/assets/images/valve-open.png" alt="" />
+          <div class="time-wrapper">
+            <p class="start-time">14:25</p>
+            <p class="end-time"></p>
+          </div>
         </div>
         <van-divider />
-        <div class="fertilizer">
-          <span>施肥</span>
-          <van-switch :value="fertilizerOpen" @input="fertilizerHandler" />
+        <div class="option-wrapper">
+          <div class="water">
+            <span>浇水</span>
+            <van-switch :value="waterOpen" @input="waterHandler" />
+          </div>
+          <van-divider />
+          <div class="fertilizer">
+            <span>施肥</span>
+            <van-switch :value="fertilizerOpen" @input="fertilizerHandler" />
+          </div>
+        </div>
+        <van-divider />
+      </div>
+      <div class="status-wrapper">
+        <p class="status">工作状态</p>
+        <div class="status-content">
+          <div class="content-item time-dis">
+            <img class="close" src="@/assets/images/water-dis.png" alt="" />
+            <div class="num">0041</div>
+            <div class="time-wrapper">
+              <p class="start-time">14:25</p>
+              <p class="end-time">14:26</p>
+            </div>
+          </div>
+          <div class="content-item time-open">
+            <img class="open" src="@/assets/images/water-open.png" alt="" />
+            <div class="num">0042</div>
+            <div class="time-wrapper">
+              <p class="start-time">14:25</p>
+              <p class="end-time"></p>
+            </div>
+          </div>
+          <div class="content-item time-open">
+            <img class="open" src="@/assets/images/water-open.png" alt="" />
+            <div class="num">0042</div>
+            <div class="time-wrapper">
+              <p class="start-time">14:25</p>
+              <p class="end-time"></p>
+            </div>
+          </div>
+          <div class="content-item time-open">
+            <img class="open" src="@/assets/images/water-open.png" alt="" />
+            <div class="num">0042</div>
+            <div class="time-wrapper">
+              <p class="start-time">14:25</p>
+              <p class="end-time"></p>
+            </div>
+          </div>
+          <div class="content-item time-open">
+            <img class="open" src="@/assets/images/water-open.png" alt="" />
+            <div class="num">0042</div>
+            <div class="time-wrapper">
+              <p class="start-time">14:25</p>
+              <p class="end-time"></p>
+            </div>
+          </div>
         </div>
       </div>
-      <van-divider />
     </div>
-    <div class="status-wrapper">
-      <p class="status">工作状态</p>
-      <div class="status-content">
-        <div class="content-item time-dis">
-          <img class="close" src="@/assets/images/water-dis.png" alt="" />
-          <div class="num">0041</div>
-          <div class="time-wrapper">
-            <p class="start-time">14:25</p>
-            <p class="end-time">14:26</p>
-          </div>
-        </div>
-        <div class="content-item time-open">
-          <img class="open" src="@/assets/images/water-open.png" alt="" />
-          <div class="num">0042</div>
-          <div class="time-wrapper">
-            <p class="start-time">14:25</p>
-            <p class="end-time"></p>
-          </div>
-        </div>
-        <div class="content-item time-open">
-          <img class="open" src="@/assets/images/water-open.png" alt="" />
-          <div class="num">0042</div>
-          <div class="time-wrapper">
-            <p class="start-time">14:25</p>
-            <p class="end-time"></p>
-          </div>
-        </div>
-        <div class="content-item time-open">
-          <img class="open" src="@/assets/images/water-open.png" alt="" />
-          <div class="num">0042</div>
-          <div class="time-wrapper">
-            <p class="start-time">14:25</p>
-            <p class="end-time"></p>
-          </div>
-        </div>
-        <div class="content-item time-open">
-          <img class="open" src="@/assets/images/water-open.png" alt="" />
-          <div class="num">0042</div>
-          <div class="time-wrapper">
-            <p class="start-time">14:25</p>
-            <p class="end-time"></p>
-          </div>
-        </div>
-      </div>
+    <div class="finished-wrapper" v-if="finished">
+      <img class="status" src="@/assets/images/water-open.png" alt="">
+      <div class="text">操作成功</div>
     </div>
-    <!-- <van-popup v-model="waterOpen" :close-on-click-overlay="false"
-      >内容</van-popup -->
   </div>
 </template>
 
@@ -81,6 +85,7 @@ export default {
     return {
       waterOpen: false, //浇水
       fertilizerOpen: false, // 施肥
+      finished: false,
     };
   },
   methods: {
@@ -89,23 +94,25 @@ export default {
       console.log("waterOpen", this.waterOpen);
       if (!this.waterOpen) {
         Dialog.confirm({
+          confirmButtonColor: "#00c200",
           title: "确认停止浇水",
           message: "请确认您已停止浇水并关闭阀门",
         })
           .then(() => {
             // on confirm
+            this.finished = true;
           })
           .catch(() => {
             // on cancel
-            this.waterOpen = true
+            this.waterOpen = true;
           });
       }
     },
 
     fertilizerHandler() {
-      this.fertilizerOpen = !this.fertilizerOpen;
       if (!this.fertilizerOpen) {
         Dialog.confirm({
+          confirmButtonColor: "#00c200",
           title: "确认关闭施肥",
           message: "本次施肥将不被系统记录",
         })
@@ -114,17 +121,22 @@ export default {
           })
           .catch(() => {
             // on cancel
-            this.fertilizerOpen = true
+            this.fertilizerOpen = true;
           });
       }
+      this.fertilizerOpen = !this.fertilizerOpen;
     },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss">
+body, html {
+  height: 100%;
+}
 .home-wrapper {
+  height: 100%;
   text-align: left;
   .design {
     padding: 0 0.16rem;
@@ -298,6 +310,28 @@ export default {
     background: #07c160;
   }
 
+  .btn-green {
+    color: #00c200 !important;
+  }
+
+  // 操作成功
+  .finished-wrapper {
+    height: 100%;
+    text-align: center;
+    background: #fff;
+    .status {
+      width: .96rem;
+      height: .96rem;
+      padding-top: .48rem;
+    }
+
+    .text {
+      padding-top: .15rem;
+      font-size: .19rem;
+      color: #000;
+    }
+  }
+
   // 弹窗
   /deep/ .van-dialog {
     border-radius: 8px;
@@ -313,8 +347,5 @@ export default {
     font-size: 0.15rem;
     border-bottom: 0.005rem solid #d2d3d5;
   }
-}
-/deep/.van-button__text {
-  color: #00c200 !important;
 }
 </style>
